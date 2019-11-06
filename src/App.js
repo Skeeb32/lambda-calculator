01901ebf21042f308908606451328afd797bfe2e
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2019-11-05 16:39:01 -0800

    Stand Up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
+
 import "./App.css";
+import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
+import Operators from "./components/ButtonComponents/OperatorButtons/Operators";
+import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
+
+
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+import NumberButton from "./components/ButtonComponents/NumberButtons/NumberButton";
+import OperatorButton from "./components/ButtonComponents/OperatorButtons/OperatorButton";
+import SpecialButton from "./components/ButtonComponents/SpecialButtons/SpecialButton";
+import Display from "./components/DisplayComponents/Display";
+
+
 
 function App() {
+
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
   // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
@@ -14,10 +27,18 @@ function App() {
   // Don't forget to pass the functions (and any additional data needed) to the components as props
 
   return (
+
+
+
     <div className="container">
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+       <Display /> 
+       <NumberButton />
+        <OperatorButton />
+        <SpecialButton />
+        
       </div>
     </div>
   );
